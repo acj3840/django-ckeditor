3122a753223149d2b843ac111468d0da092f5884
alex_jones <alex_jones@wgbh.org>
2014-06-12 10:45:30 -0400

    changed the admin to just broadcast an even that plugins can wire to, rather than hard code the solution into the admin

--- ckeditor/static/admin/js/inlines.js
+++ ckeditor/static/admin/js/inlines.js
@@ -250,22 +250,12 @@
             });
         };
 
-        var fixImpossibleToUseIFrames = function (row) {
-            var targetTextArea = $('#id_' + row.attr('id') + '-text');
-            var targetP = $(targetTextArea.parent());
-            targetP.children('#cke_id_' + row.attr('id') + '-text').remove();
-
-            var newScript = $('<script />');
-            newScript.prop('type', 'text/javascript');
-            newScript.html('CKEDITOR.replace("id_' + row.attr('id') + '-text", {"filebrowserWindowWidth": 940, "toolbar_Basic": ' +
-                '[["Source", "-", "Bold", "Italic"]], "language": "en-us", "toolbar_Full": ' +
-                '[["Styles", "Format", "Bold", "Italic", "Underline", "Strike", "SpellChecker", "Undo", "Redo"], ' +
-                '["Image", "Flash", "Table", "HorizontalRule"], ["TextColor", "BGColor"], ["Smiley", "SpecialChar"], ' +
-                '["Source"]], "filebrowserUploadUrl": "/ckeditor/upload/", "height": 291, "width": 835, ' +
-                '"filebrowserBrowseUrl": "/ckeditor/browse/", "skin": "moono", "filebrowserWindowHeight": 725, ' +
-                '"toolbar": "Full"});');
-
-            targetP.append(newScript);
+        var fireRowCreatedEvent = function (row) {
+            $(window).trigger('row-created', [
+                {
+                    row: row
+                }
+            ]);
         };
 
         $rows.formset({
@@ -281,7 +271,7 @@
                 reinitDateTimeShortCuts();
                 updateSelectFilter();
                 updateInlineLabel(row);
-                fixImpossibleToUseIFrames(row);
+                fireRowCreatedEvent(row);
             })
         });
 
